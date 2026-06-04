d0092027aac94bd8393b762d3125ea2c926367fd
Rumaisa <rumaisanaved@gmail.com>
2026-06-04 13:08:28 +0500

    FIX: Fixed the types of lifestyle service.

--- src/app/core/services/lifestyle/lifestyle.service.ts
+++ src/app/core/services/lifestyle/lifestyle.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LIFESTYLE_API_URL } from '../tokens';
+import { LifeStyleData } from '../../interfaces/lifestyle';
+import { ApiResponse, BaseApiResponse } from '../../interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -10,11 +12,11 @@ export class LifestyleService {
   private http = inject(HttpClient);
   private apiUrl = `${inject(LIFESTYLE_API_URL)}`;
 
-  get(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+  get(): Observable<ApiResponse<LifeStyleData>> {
+    return this.http.get<ApiResponse<LifeStyleData>>(this.apiUrl);
   }
 
-  update(data: any): Observable<any> {
-    return this.http.put<any>(this.apiUrl, data);
+  update(data: any): Observable<BaseApiResponse> {
+    return this.http.put<BaseApiResponse>(this.apiUrl, data);
   }
 }

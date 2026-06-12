6e65c1b53adc450ee6fffb38c78e087bd89adf78
Rumaisa <rumaisanaved@gmail.com>
2026-06-12 13:27:35 +0500

    REFACTOR: Removed unnecessary insurance edit form component.

--- src/app/components/insurance/insurance-edit-form/insurance-edit-form.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-insurance-edit-form',
-  imports: [],
-  templateUrl: './insurance-edit-form.component.html',
-  styleUrl: './insurance-edit-form.component.css',
-})
-export class InsuranceEditFormComponent {}
